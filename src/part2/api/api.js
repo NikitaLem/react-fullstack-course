2b44ce7f280497ca5ba6e406c1f2d7f605e6210d
Никита Лемешенко <n.lemeshenko@magdv.com>
2019-12-05 09:55:14 +0700

    .env added

--- src/part2/api/api.js
+++ src/part2/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://infinite-ridge-86850.herokuapp.com/api/persons',
+  baseURL: `${process.env.API_URL}/persons`,
 });
 
 export default {
